5928f9042893239c007389910b840f07e2845b11
Jovidev <Vitorskyline17@gmail.com>
2025-03-05 14:45:35 -0300

    Creating the Categories module

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const contactsController = require('./app/controller/contactController');
+const CategoryController = require('./app/controller/categoryController');
 
 const router = Router();
 
@@ -9,5 +10,10 @@ router.get('/contacts/:id', contactsController.show);
 router.delete('/contacts/:id', contactsController.delete);
 router.post('/contacts', contactsController.store);
 router.put('/contacts/:id', contactsController.update);
+
+
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
+
 
 module.exports = router;
